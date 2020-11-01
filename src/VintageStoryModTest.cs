5dbe89776f6a4ad164418b6fcae9a5a1bf357035
DavidCarlyn <davidecarlyn@gmail.com>
2020-11-01 14:47:33 -0500

    starting vintage story modding

---
+++ src/VintageStoryModTest.cs
@@ -0,0 +1,24 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+namespace VintageStoryMods
+{
+	public class VintageStoryModTest : ModSystem
+	{
+		public override void Start(ICoreAPI api)
+		{
+			api.RegisterBlockBehaviorClass(InstaTNTBehavior.NAME, typeof(InstaTNTBehavior));
+		}
+		
+		public override void StartClientSide(ICoreClientAPI api)
+		{
+			
+		}
+		
+		public override void StartServerSide(ICoreServerAPI api)
+		{
+			
+		}
+	}
+}
